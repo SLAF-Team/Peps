124312b75fd5fe2c5fa60efeb727d1ba9c528e44
VioMrqs <violette.marquis@gmail.com>
2022-03-15 16:59:49 +0100

    add jotai for state management

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,40 @@
-import '../styles/globals.css'
-import Layout from '../components/layout';
-import Cookies from 'js-cookie';
+import "../styles/globals.css";
+import Layout from "../components/layout";
+import Cookies from "js-cookie";
+import { useAtom } from "jotai";
+import { userAtom } from "../lib/store";
+import { useEffect } from "react";
+import axios from "axios";
+import { Provider } from "jotai";
 
 function MyApp({ Component, pageProps }) {
+  const [user, setUser] = useAtom(userAtom);
+  const token = Cookies.get("token");
+
   console.log("token");
-  console.log(Cookies.get("token"))
+  console.log(token);
+
+  async function getUser() {
+    const result = await axios.get("/api/user/getCurrentUser", {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    setUser(result.data.user);
+  }
+
+  useEffect(() => {
+    getUser();
+  }, [token]);
+
+  console.log("user from app");
+  console.log(user);
 
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
 
-export default MyApp
+export default MyApp;
