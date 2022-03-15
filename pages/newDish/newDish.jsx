f2a5875c35c9944f6c04ddc77685ebae309f4f8b
Luucas51 <oudartlucas51@gmail.com>
2022-03-15 15:36:45 +0100

    resolve

---
+++ pages/newDish/newDish.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useState } from 'react/cjs/react.production.min';
+
+const newDish = () => {
+
+    const [title, setTitle] = useState();
+    const [description, setDescription] = useState();
+    const [recipe, setRecipe] = useState();
+    const [region, setRegion] = useState():
+    const [regionId, setRegionId]
+
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default newDish;
