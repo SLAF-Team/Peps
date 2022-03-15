6a0c29d513ce60942b51e82fb071ecc24ef1a6e2
Luucas51 <oudartlucas51@gmail.com>
2022-03-15 17:28:25 +0100

    resolve fix

--- pages/api/dish/addDish.js
+++ pages/api/dish/addDish.js
@@ -4,13 +4,14 @@ import prisma from "../../../lib/prisma.ts";
 
 export default async (req, res) => {
   const data = req.body;
+  console.log(data)
   try {
-    const user = await prisma.dish.create({
+    const dish = await prisma.dish.create({
       data: {
         ...data,
       },
     });
-    res.status(200).json({ dish });
+    res.status(200).json( dish );
   } catch (err) {
     console.log(err);
     res.status(403).json({ err: "Error occured while adding a new dish." });
