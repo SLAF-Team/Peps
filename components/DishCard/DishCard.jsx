b1548a54ee14bb36d680ef911e78a94751e05b93
Luucas51 <oudartlucas51@gmail.com>
2022-03-16 15:22:49 +0100

    get info dish good without recipes

--- components/DishCard/DishCard.jsx
+++ components/DishCard/DishCard.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useState } from 'react';
 
 const DishCard = (props) => {
-
     const dish = props.dish;
 
+    console.log(dish.recipes)
+
+
+    // console.log(recipe)
     
     return (
         <div>
             <h1>Titre : {dish.title}</h1>
             <p>Description : {dish.description}</p>
-            <button>Plus d'info</button>
+            <p>Recipe : {dish.recipes.map(recipe => {
+                {recipe.values()}
+            })}</p>
+
         </div>
     );
 };
