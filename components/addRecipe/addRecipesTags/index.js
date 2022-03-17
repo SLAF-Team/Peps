271beaeb5f3eb2ccfbaf367bddff85dc5a11ed5b
VioMrqs <violette.marquis@gmail.com>
2022-03-17 14:39:17 +0100

    almost ready for tags

--- components/addRecipe/addRecipesTags/index.js
+++ components/addRecipe/addRecipesTags/index.js
@@ -2,8 +2,7 @@ import { useState, useEffect } from "react/cjs/react.development";
 import axios from "axios";
 import { CheckboxGroup, Checkbox } from '@mantine/core';
 
-
-const AddRecipesTags = () => {
+const AddRecipesTags = ({recipe}) => {
 const [tags, setTags] = useState(null);
 const [value, setValue] = useState([]);
 
@@ -16,7 +15,51 @@ useEffect(() => {
   getAllTags();
 }, []);
 
+  async function addTagsToRecipe() {
+    const result = await axios.put(
+      "/api/recipe/editRecipe",
+      {
+        id: recipe.id,
+      },
+      { headers: { Authorization: `Bearer ${token}` } }
+    );
+    console.log(result)
+  }
+
+const handleClick = () => {
+  addTagsToRecipe();
+}
+
+const assignCategories = await prisma.post.create({
+  data: {
+    title: "How to be Bob",
+    categories: {
+      create: [
+        {
+          assignedBy: "Bob",
+          assignedAt: new Date(),
+          category: {
+            connect: {
+              id: 9,
+            },
+          },
+        },
+        {
+          assignedBy: "Bob",
+          assignedAt: new Date(),
+          category: {
+            connect: {
+              id: 22,
+            },
+          },
+        },
+      ],
+    },
+  },
+});
+
 return (
+  <>
     <CheckboxGroup value={value} onChange={setValue}       
       color="cyan"
       label="Tags"
@@ -27,6 +70,8 @@ return (
           <Checkbox value={tag.id} label={tag.name}/>
         )) : null}
     </CheckboxGroup>
+    <button onClick={handleClick}>Valider mon choix</button>
+    </>
   );
 }
 
---
+++ pages/api/recipe/editRecipe.js
@@ -0,0 +1,27 @@
+import prisma from "../../../lib/prisma.ts";
+import { checkAuth } from "../../../lib/auth";
+
+export default async (req, res) => {
+  console.log(req);
+  // const isAuth = await checkAuth(req);
+  // if (!isAuth) {
+  //   res.status(403).json({ err: "Forbidden" });
+  //   return;
+  // }
+
+  const data = req.body;
+
+  try {
+    const result = await prisma.recipe.update({
+      where: {
+        id: data.id,
+      },
+      data: {
+        ...data,
+      },
+    });
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(400).json({ err: "Error while updating." });
+  }
+};
