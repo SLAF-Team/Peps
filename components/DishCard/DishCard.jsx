fe4c0047d06ebfdc04e8dd5d0536c860c91c343b
Luucas51 <oudartlucas51@gmail.com>
2022-03-17 10:05:08 +0100

    Edit dish good, last bug : write impossible in form

--- components/DishCard/DishCard.jsx
+++ components/DishCard/DishCard.jsx
@@ -18,6 +18,29 @@ const DishCard = (props) => {
         }
       }
 
+      async function editDish() {
+        const result = await axios.put(
+          "/api/dish/editDish",
+          {
+            title: dish.title,
+            description:  dish.description,
+          },
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+      }
+
+      const [state, setState] = useState({
+		dishTitle: dish.title,
+		dishDescription: dish.description,
+        });
+
+      const handleValueChange = (e) => {
+		setState({
+			...state,
+			[e.target.name]: e.target.value,
+		});
+	};
+
     return (
         <div>
             <h1>Titre : {dish.title}</h1>
@@ -25,9 +48,31 @@ const DishCard = (props) => {
             <button onClick={deleteDish} >
                 Supprimer
             </button>
-            <button onClick={editDish}>
-                modifier
-            </button>
+
+
+            <form >
+								<label>Title</label>
+								<input
+									name="shackTitle"
+									type="text"
+									value={state.dishTitle}
+									onChange={handleValueChange}
+								/>
+								<label>Description</label>
+								<textarea
+									name="shackDescription"
+									type="text"
+									style={{ width: "100%", height: "100px" }}
+									value={state.dishDescription}
+									onChange={handleValueChange}
+								/>
+                                      <button type="submit" onClick={editDish}>
+                                            J'édite
+                                        </button>
+					</form>
+
+
+
             <div>
                 {dish.recipes.map(recipe => (
                     <p>{recipe.name}</p>
