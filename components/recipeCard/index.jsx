93eb26bf115c34361db36d0cc7993b314fb3f2de
VioMrqs <violette.marquis@gmail.com>
2022-03-18 11:15:28 +0100

    add structure for likes

--- components/recipeCard/index.jsx
+++ components/recipeCard/index.jsx
@@ -1,21 +1,40 @@
 import { get } from "http";
 import { useUserContext } from "../../context/UserContext";
-import { useState, useEffect } from "react";
+import Button from "../Button";
 
 const RecipeCard = ({ recipe }) => {
   const { user } = useUserContext();
 
   const isLiked = user?.likes.some((like) => like.recipeId === recipe.id);
 
-  console.log(recipe)
+  console.log(recipe);
+
+const handleDeleteLike = () => {
+
+}
+
+const handleCreateLike = () => {};
   return (
     <>
       <h1>{recipe.name}</h1>
       <h2>{recipe.description}</h2>
       <h2>{recipe.cook.name}</h2>
       {recipe.likes && <h2>Likes: {recipe._count.likes}</h2>}
-      {/* check si c'est déjà liké */}
-      <button>{isLiked ? "true" : "false"}</button>
+      {isLiked ? (
+        <Button
+          className="success"
+          label="liké"
+          href=""
+          onClick={handleDeleteLike}
+        />
+      ) : (
+        <Button
+          className="primary"
+          label="like!"
+          href=""
+          onClick={handleCreateLike}
+        />
+      )}
     </>
   );
 };
