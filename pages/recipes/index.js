a8a5de6b76a7145bd6ee2d96265b97d0c06413e5
Antoine Ortega <asseorteg@gmail.com>
2022-03-18 10:42:13 +0100

    Missing some alignments

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -2,25 +2,41 @@ import React from "react";
 import { useState, useEffect } from "react/cjs/react.development";
 import axios from "axios";
 import RecipeCard from "../../components/recipeCard";
+import prisma from "../../lib/prisma.ts";
 
-const Recipes = () => {
-  const [recipes, setRecipes] = useState(null);
+const Recipes = (props) => {
+  const recipes = props.recipes;
+  // const [recipes, setRecipes] = useState(null);
 
   // get recipes
-  async function getAllRecipes() {
-    const result = await axios.get("/api/recipe/getRecipes");
-    setRecipes(result.data);
-  }
+  // async function getAllRecipes() {
+  //   const result = await axios.get("/api/recipe/getRecipes");
+  //   setRecipes(result.data);
+  // }
 
-  useEffect(() => {
-    getAllRecipes();
-  }, []);
+  // useEffect(() => {
+  //   getAllRecipes();
+  // }, []);
+
+  console.log(recipes);
 
   return (
-    recipes && recipes.map((recipe) => (
-      <RecipeCard recipe={recipe} key={recipe.id}/>
-    ))
+    recipes &&
+    recipes.map((recipe, i) => <RecipeCard recipe={recipe} key={i} />)
   );
 };
+
+export async function getServerSideProps() {
+  const allRecipes = await prisma.recipe.findMany({
+    include: {
+      cook: { select: { email: true, name: true, id: true } },
+    },
+  });
+  return {
+    props: {
+      recipes: allRecipes,
+    },
+  };
+}
 
 export default Recipes;
