9e09a4060e9e2d3fa1994788a685cb3bbb3f84c7
VioMrqs <violette.marquis@gmail.com>
2022-03-18 15:00:15 +0100

    cleaned session dish

--- components/DishCard/DishCard.jsx
+++ components/DishCard/DishCard.jsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import { useState, useRef } from 'react';
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import { useRouter } from 'next/router';
-import Button from '../Button';
+import Button from "../Button";
 
-const DishCard = (props) => {
-    const dish = props.dish;
-    const token = Cookies.get('token');
-    const router = useRouter();
-    const formRef = useRef();
+const DishCard = ({dish}) => {
 
-
-
-    return (
-        <div>
-            <h1>Titre : {dish.title}</h1>
-            <p>Description : {dish.description}</p>
-            <Button type={'primary'} label={"Plus de détail"} href={`/dishes/${dish.id}`}/>
-        </div>
-    );
-
+return (
+<div>
+    <h1>Titre : {dish.title}</h1>
+    <p>Description : {dish.description}</p>
+    <Button
+    type={"primary"}
+    label={"Plus de détail"}
+    href={`/dishes/${dish.id}`}
+    />
+</div>
+);
 };
 
 export default DishCard;
