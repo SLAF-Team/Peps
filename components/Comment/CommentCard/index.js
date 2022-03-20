a0a2764892a3bdaf6fafa773a8e41c6c9113a80a
VioMrqs <violette.marquis@gmail.com>
2022-03-20 20:08:59 +0100

    delete ok

--- components/Comment/CommentCard/index.js
+++ components/Comment/CommentCard/index.js
@@ -1,9 +1,55 @@
+import Button from "../../Button";
+import classes from "./CommentCard.module.css";
+import axios from "axios";
+import Cookies from "js-cookie";
+import { useUserContext } from "../../../context/UserContext";
+
 const CommentCard = ({ comment }) => {
+  const token = Cookies.get("token");
+  const { user } = useUserContext();
+
+  //todo
+  // Ajouter un routing nested
+  // Ajouter update
+  // CSS
+
+  const isAuthor = comment.userId == user?.id ? true : false;
+
+  async function deleteComment() {
+    if (window.confirm("Souhaitez vous supprimer ce commentaire?")) {
+      await axios.delete(`/api/comment/delete/${comment?.id}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+      // router.push("/recipes/");
+    }
+  }
+
   return (
-    <>
+    <div>
       <h3>{comment.user?.name}</h3>
       <p>{comment.text}</p>
-    </>
+      {isAuthor && (
+        <div className={classes.detailscontainer}>
+          <div className={classes.editcontainer}>
+            <Button
+              label="Supprimer"
+              type="danger"
+              handleClick={() => deleteComment()}
+              href="#"
+              className={classes.button}
+            />
+            <br></br>
+            <Button
+              label="Editer"
+              type="warning"
+              handleClick={() => editComment()}
+              href="#"
+              className={classes.button}
+            />
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
