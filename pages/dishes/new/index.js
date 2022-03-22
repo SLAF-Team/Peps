4e7ba84b44a0bdbd5558d9aa9ab83dc00401a2df
VioMrqs <violette.marquis@gmail.com>
2022-03-22 17:16:52 +0100

    front validation ok

--- pages/dishes/new/index.js
+++ pages/dishes/new/index.js
@@ -4,13 +4,20 @@ import { useRef } from "react/cjs/react.development";
 import axios from "axios";
 import prisma from "../../../lib/prisma.ts";
 import Cookies from "js-cookie";
+import { useUserContext } from "../../../context/UserContext";
+import { checkAdminAuth, checkLogAuth } from "../../../lib/authfront";
 
-const newDish = ({regions}) => {
+const newDish = ({ regions }) => {
+  const { user } = useUserContext();
   const formRef = useRef();
   const token = Cookies.get("token");
   const [disable, setDisable] = useState(false);
+  const [auth, setAuth] = useState(false);
 
-  // add Dish
+  useEffect(() => {
+    if (checkAdminAuth(user) && checkLogAuth(user)) setAuth(true);
+  }, [user]);
+
   async function addNewDish(params) {
     setDisable(true);
     const { addTitle, addDescription, addRegion } = formRef.current;
@@ -29,37 +36,51 @@ const newDish = ({regions}) => {
     setDisable(false);
   }
 
-  return (
-    <div>
-      <form ref={formRef}>
-        <div>
-          <label>Titre</label>
-          <input name="addTitle" type="text" />
-        </div>
-        <div>
-          <label>Description</label>
-          <input name="addDescription" type="text" />
-        </div>
-        {regions ? (
+  if (auth) {
+    return (
+      <div>
+        <form ref={formRef}>
           <div>
-            <label>Region</label>
-            <select name="addRegion">
-              {regions.map((region) => (
-                <option value={region.id} key={region.id}>{region.name}</option>
-              ))}
-            </select>
+            <label>Titre</label>
+            <input name="addTitle" type="text" />
           </div>
-        ) : null}
-        <button
-          disabled={disable}
-          className="btn btn-primary my-3"
-          onClick={() => addNewDish()}
-        >
-          Créer un plat
-        </button>
-      </form>
-    </div>
-  );
+          <div>
+            <label>Description</label>
+            <input name="addDescription" type="text" />
+          </div>
+          {regions ? (
+            <div>
+              <label>Region</label>
+              <select name="addRegion">
+                {regions.map((region) => (
+                  <option value={region.id} key={region.id}>
+                    {region.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          ) : null}
+          <button
+            disabled={disable}
+            className="btn btn-primary my-3"
+            onClick={() => addNewDish()}
+          >
+            Créer un plat
+          </button>
+        </form>
+      </div>
+    );
+  } else {
+    return (
+      <p>
+        Veuillez vous{" "}
+        <b>
+          <a href="/login/">connecter</a>
+        </b>{" "}
+        pour écrire un commentaire
+      </p>
+    );
+  }
 };
 
 export async function getServerSideProps() {
