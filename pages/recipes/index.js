f5a3b97cd27ec585d7914b4f3266a253afa763ed
VioMrqs <violette.marquis@gmail.com>
2022-03-22 13:29:01 +0100

    ingredient ok, add nice design touch

--- pages/recipes/index.js
+++ pages/recipes/index.js
@@ -4,23 +4,33 @@ import prisma from "../../lib/prisma.ts";
 import classes from "./Recipe.module.css";
 import { useState, useEffect } from "react";
 import { MultiSelect } from "@mantine/core";
+import { Switch } from "@mantine/core";
 import axios from "axios";
 
 const Recipes = ({ recipes, tags, countries, types, ingredients }) => {
+  // set up state for multiselect
+
   const idTags = [];
   tags?.map((element) => idTags.push(element.id));
   const [filterTag, setFilterTag] = useState(idTags);
+
   const idCountries = [];
   countries?.map((element) => idCountries.push(element.id));
   const [filterCountry, setFilterCountry] = useState(idCountries);
+
   const idTypes = [];
   types?.map((element) => idTypes.push(element.id));
   const [filterType, setFilterType] = useState(idTypes);
+
   const idIngredients = [];
   ingredients?.map((element) => idIngredients.push(element.id));
   const [filterIngredient, setFilterIngredient] = useState(idIngredients);
+
   const [filteredRecipes, setFilterRecipes] = useState(recipes);
 
+  const [filter, setFilter] = useState(true);
+
+  // set up data for multiselect
   const dataTags = [];
   tags?.map((tag) => dataTags.push({ value: tag.id, label: tag.name }));
 
@@ -37,28 +47,7 @@ const Recipes = ({ recipes, tags, countries, types, ingredients }) => {
     dataIngredients.push({ value: ingredient.id, label: ingredient.name })
   );
 
-  useEffect(() => {
-    const data = {
-      where: {
-        countryId: {
-          in: filterCountry,
-        },
-        typeId: {
-          in: filterType,
-        },
-        tags: {
-          some: { id: { in: filterTag } },
-        },
-        // ingredientsUnit: {
-        //   // where: {
-        //     ingredientID: { in: filterIngredient },
-        //   // },
-        // },
-      },
-    };
-    getRecipes(data);
-  }, [filterTag, filterCountry, filterType]);
-
+  // async search fonction
   const getRecipes = async (data) => {
     try {
       const result = await axios.post(`/api/recipe/searchRecipes`, {
@@ -72,37 +61,89 @@ const Recipes = ({ recipes, tags, countries, types, ingredients }) => {
     }
   };
 
+  // change filter state
+  const handleChange = () => {
+    setFilter(!filter);
+  };
+
+  //useEffect for filter with exception if filter not used (ternaire <3)
+  useEffect(() => {
+    const data = filter
+      ? {
+          where: {
+            countryId: {
+              in: filterCountry,
+            },
+            typeId: {
+              in: filterType,
+            },
+            tags: {
+              some: { id: { in: filterTag } },
+            },
+            ingredientsUnit: {
+              some: {
+                ingredientId: { in: filterIngredient },
+              },
+            },
+          },
+        }
+      : null;
+    getRecipes(data);
+  }, [filterTag, filterCountry, filterType, filterIngredient, filter]);
+
   return (
     <div className={classes.margin}>
-      <div className="row">
-        <MultiSelect
-          data={dataTags}
-          value={filterTag}
-          onChange={setFilterTag}
-          placeholder="tags"
-        />
-        <MultiSelect
-          data={dataCountries}
-          value={filterCountry}
-          onChange={setFilterCountry}
-          placeholder="pays"
-        />
-        <MultiSelect
-          data={dataTypes}
-          value={filterType}
-          onChange={setFilterType}
-          placeholder="types"
-        />
-        <MultiSelect
-          data={dataIngredients}
-          value={filterIngredient}
-          onChange={setFilterIngredient}
-          placeholder="ingrédients"
-        />
-        {filteredRecipes &&
-          filteredRecipes.map((recipe, i) => (
-            <RecipeCard recipe={recipe} key={i} col="col-3" />
-          ))}
+      <div className="column">
+        <div className="row">
+          <Switch
+            label="Utiliser les filtres"
+            checked={filter}
+            onChange={handleChange}
+            color="cyan"
+          />
+          {filter && (
+            <>
+              <MultiSelect
+                data={dataTags}
+                value={filterTag}
+                onChange={setFilterTag}
+                placeholder="tags"
+                searchable
+                clearable
+              />
+              <MultiSelect
+                data={dataCountries}
+                value={filterCountry}
+                onChange={setFilterCountry}
+                placeholder="pays"
+                searchable
+                clearable
+              />
+              <MultiSelect
+                data={dataTypes}
+                value={filterType}
+                onChange={setFilterType}
+                placeholder="types"
+                searchable
+                clearable
+              />
+              <MultiSelect
+                data={dataIngredients}
+                value={filterIngredient}
+                onChange={setFilterIngredient}
+                placeholder="ingrédients"
+                searchable
+                clearable
+              />
+            </>
+          )}
+        </div>
+        <div className="row">
+          {filteredRecipes &&
+            filteredRecipes.map((recipe, i) => (
+              <RecipeCard recipe={recipe} key={i} col="col-3" />
+            ))}
+        </div>
       </div>
     </div>
   );
