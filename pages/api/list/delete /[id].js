b841a0e23876217503f195d7cf82433b21a006b3
VioMrqs <violette.marquis@gmail.com>
2022-03-22 18:23:16 +0100

    working on list

--- pages/api/list/delete /[id].js
+++ pages/api/list/delete /[id].js
@@ -1,24 +1,26 @@
-import prisma from "../../../../lib/prisma.ts"
+import prisma from "../../../../lib/prisma.ts";
 import { checkAuth } from "../../../../lib/auth";
 
 export default async (req, res) => {
   const isAuth = await checkAuth(req);
-  if(!isAuth){
+  if (!isAuth) {
     res.status(403).json({ err: "Forbidden" });
     return;
   }
 
-  const {id} = req.query
+  const { id } = req.query;
 
   try {
-    const deleteComment = await prisma.comment.delete({
+    const deleteList = await prisma.list.delete({
       where: {
         id: parseInt(id),
       },
     });
-    res.status(200).json(deleteComment);
+    res.status(200).json(deleteList);
   } catch (error) {
     console.log(error);
-    res.status(400).json({ err: "Error occured while deleting a Recipe item." });
+    res
+      .status(400)
+      .json({ err: "Error occured while deleting a Recipe item." });
   }
 };
--- pages/lists/[id].jsx
+++ pages/lists/[id].jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import UserList from "../../components/UserList";
-import styles from "./Lists.module.css";
+import classes from "./Lists.module.css";
 import RecipeCard from "../../components/recipeCard";
 import prisma from "../../lib/prisma.ts";
 import FilterSelector from "../../components/FilterSelector";
+import { checkAuthorAuth } from "../../lib/authfront";
+import { useUserContext } from "../../context/UserContext";
+import Button from "../../components/Button";
+import Cookies from "js-cookie";
 
-const Profile = ({ lists }) => {
-  const [style, setStyle] = useState(false);
+const Profile = ({ list }) => {
+  const {user} = useUserContext();
+  const token = Cookies.get("token")
 
   const handleClickLeft = () => {
     setStyle(false);
@@ -16,22 +21,57 @@ const Profile = ({ lists }) => {
     setStyle(true);
   };
 
+  async function deleteList() {
+      if (window.confirm("Souhaitez vous supprimer cette liste?")) {
+        await axios.delete(`/api/list/delete/${list?.id}`, {
+          headers: { Authorization: `Bearer ${token}` },
+        });
+      }
+    }
+
+  const handleLeleteList = () => {
+    //notif
+    deleteList();
+    //notif
+  }
+
+    const updateList = () => {};
+
   return (
     <>
-      <UserList user={lists} color="#26c485" />
-      <FilterSelector left={lists?.recipes.length} />
+      <UserList user={list} color="#26c485" />
+      <FilterSelector left={list?.recipes.length} />
       <div className="row">
-        {lists?.recipes.map((recipe) => (
+        {list?.recipes.map((recipe) => (
           <RecipeCard recipe={recipe} col="col-3" />
         ))}
       </div>
+      {checkAuthorAuth(user, list) && (
+        <>
+          <Button
+            label="Supprimer"
+            type="danger"
+            handleClick={() => handleLeleteList()}
+            href="#"
+            className={classes.button}
+          />
+          <br></br>
+          <Button
+            label="Editer"
+            type="warning"
+            handleClick={() => updateList()}
+            href="#"
+            className={classes.button}
+          />
+        </>
+      )}
     </>
   );
 };
 
 export async function getServerSideProps(context) {
   const { id } = context.params;
-  const allLists = await prisma.list.findUnique({
+  const oneList = await prisma.list.findUnique({
     where: { id: parseInt(id) },
     include: {
       user: { select: { name: true } },
@@ -40,7 +80,7 @@ export async function getServerSideProps(context) {
   });
   return {
     props: {
-      lists: allLists,
+      list: oneList,
     },
   };
 }
