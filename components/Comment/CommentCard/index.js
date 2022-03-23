21e7e278c1cb5ac9e82aad17eebf8eadcdf24128
VioMrqs <violette.marquis@gmail.com>
2022-03-23 15:17:40 +0100

    add moment + delete comment working

--- components/Comment/CommentCard/index.js
+++ components/Comment/CommentCard/index.js
@@ -3,48 +3,60 @@ import classes from "./CommentCard.module.css";
 import axios from "axios";
 import Cookies from "js-cookie";
 import { useUserContext } from "../../../context/UserContext";
-import cross from "../../../assets/images/cross.svg"
+import cross from "../../../assets/images/cross.svg";
 import Image from "next/image";
+import moment from "moment";
+import { useNotifications } from "@mantine/notifications";
+import { useRouter } from "next/router";
 
 const CommentCard = ({ comment }) => {
+  const router = useRouter();
   const token = Cookies.get("token");
   const { user } = useUserContext();
-
+  const notifications = useNotifications();
   const isAuthor = comment.userId == user?.id ? true : false;
+  const recipeId = comment.recipeId
+  console.log(recipeId)
 
-  //todo : check si ça marche
   async function deleteComment() {
     if (window.confirm("Souhaitez vous supprimer ce commentaire?")) {
       await axios.delete(`/api/comment/delete/${comment?.id}`, {
         headers: { Authorization: `Bearer ${token}` },
       });
-      // router.push("/recipes/");
+      router.push(`/recipes/${recipeId}`)
+      notifications.showNotification({
+        title: "Bravo",
+        message: "Votre commentaire a bien été supprimé.",
+        color: "red",
+      });
     }
   }
 
   return (
-      <div className={classes.modulecontainer}>
-        <div className={classes.avatarcontainer}>
-          <div
-            className={classes.avatar}
-            style={{ backgroundColor: "#ffd12f" }}
-          >
-            <span className={classes.letter}>
-              {user?.name[0].toUpperCase()}
-            </span>
-          </div>
+    <div className={classes.modulecontainer}>
+      <div className={classes.avatarcontainer}>
+        <div className={classes.avatar} style={{ backgroundColor: "#ffd12f" }}>
+          <span className={classes.letter}>{user?.name[0].toUpperCase()}</span>
         </div>
-        <div className={classes.textcontainer}>
-          <p className={classes.commentauthor}>{comment.user?.name}</p>
-          <p className={classes.commentauthor}>{comment.text}</p>
-        </div>
-        {isAuthor && (
-          <div className={classes.editcontainer}>
-            <Image src={cross} width={12} height={12} onClick={() => deleteComment()}/>
-            <br></br>
-          </div>
-        )}
       </div>
+      <div className={classes.textcontainer}>
+        <p className={classes.commentauthor}>
+          {comment.user?.name} - {moment(comment?.createdAt).fromNow()}
+        </p>
+        <p className={classes.commentauthor}>{comment.text}</p>
+      </div>
+      {isAuthor && (
+        <div className={classes.editcontainer}>
+          <Image
+            src={cross}
+            width={12}
+            height={12}
+            onClick={() => deleteComment()}
+          />
+          <br></br>
+        </div>
+      )}
+    </div>
   );
 };
 
