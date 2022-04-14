81e4baae94bb0a6adfa2bbdc5fef240c16769c8e
VioMrqs <violette.marquis@gmail.com>
2022-04-14 17:18:54 +0200

    displaying rate if rated

--- components/Rating/index.jsx
+++ components/Rating/index.jsx
@@ -11,7 +11,11 @@ const Rating = ({ recipe }) => {
   const [rating, setRating] = useState(null);
   const [isRated, setIsRated] = useState(false);
   const [recipeRatings, setRecipeRatings] = useState([]);
-  const [userRatings, setUserRatings] = useState([]);
+  const [userHasRated, setUserHasRated] = useState(false);
+  const [userRating, setUserRating] = useState(null);
+
+  console.log("user rating");
+  console.log(userHasRated);
 
   useEffect(() => {
     setRecipeRatings(recipe.ratings);
@@ -19,10 +23,58 @@ const Rating = ({ recipe }) => {
 
   useEffect(() => {
     if (user) {
-      setUserRatings(user.ratings);
+      setUserHasRated(
+        user.ratings.filter((element) => element.recipeId === recipe.id)
+          ? true
+          : false
+      );
     }
   }, [user]);
 
+  useEffect(() => {
+    if (userHasRated) {
+      setUserRating(
+        user.ratings.filter((element) => element.recipeId === recipe.id)[0]
+          .rating
+      );
+    }
+  }, [userHasRated]);
+
+  if (userHasRated) {
+    return (
+      <>
+        Vous avez noté cette recette
+        <div className={styles.rating__box}>
+          {Array(userRating)
+            .fill(1)
+            .map((element, index) => (
+              <div className={styles.rating__star}>
+                <Image
+                  src={starfill}
+                  width={30}
+                  height={30}
+                  value={index + 1}
+                  key={index + 1}
+                />
+              </div>
+            ))}
+          {Array(5 - userRating)
+            .fill(1)
+            .map((element, index) => (
+              <div className={styles.rating__star}>
+                <Image
+                  src={starblack}
+                  width={30}
+                  height={30}
+                  value={userRating + index + 1}
+                  key={userRating + index + 1}
+                />
+              </div>
+            ))}
+        </div>
+      </>
+    );
+  }
   return (
     <>
       Noter cette recette
