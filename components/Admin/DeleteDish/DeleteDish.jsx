98d0b5672ad24443193dd4bd6102f87ffe082d72
Luucas51 <oudartlucas51@gmail.com>
2022-04-19 16:27:34 +0200

    Remove dish from admin space with re render

---
+++ components/Admin/DeleteDish/DeleteDish.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import Button from '../../Button';
+import Cookies from 'js-cookie'
+import Router from 'next/router';
+
+const DeleteDish = ({id, onDelete}) => {
+
+    const token = Cookies.get('token')
+
+    const deleteDish = async () => {
+        const result = await axios.delete(`/api/dish/delete/${id}`, {
+            headers: { Authorization: `Bearer ${token}` }
+        });
+        console.log(result)
+        onDelete()
+    }
+
+    return (
+        <div>
+            <Button
+                label="Supprimer le plat"
+                handleClick={() => deleteDish()}
+                href="#"
+                type="danger"
+            />
+        </div>
+    );
+};
+
+export default DeleteDish;
--- pages/admin/dishes/index.jsx
+++ pages/admin/dishes/index.jsx
@@ -4,12 +4,13 @@ import Button from '../../../components/Button'
 import DishCard from '../../../components/DishCard/DishCard';
 import Cookies from 'js-cookie';
 import Router from 'next/router';
+import DeleteDish from '../../../components/Admin/DeleteDish/DeleteDish';
+import classes from './index.module.css'
 
 const AllDishes = () => {
 
     const [data, setData]= useState();
     const [loaded, setLoaded] = useState(false);
-    const token = Cookies.get('token')
 
     const getDishes = async() => {
         const result = await axios.get('/api/dish/getDishes');
@@ -19,32 +20,21 @@ const AllDishes = () => {
 
     useEffect(() => {
         getDishes()
-    }, [])
+    }, []);
 
-
-    const deleteDish = async(id) => {
-        const result = await axios.delete(`/api/dish/delete/${id}`, {
-            headers: { Authorization: `Bearer ${token}` }
-        });
-        console.log(result)
-        Router.push('/admin')
-    }
+    const handleDishUpdate = () => {
+        getDishes();
+    };
 
     return (
         <div>
             <h1>Dishes</h1>
             {loaded && data.map((dish, i) => (
-                <div key={i}>
+                <div key={i} className={classes.card}>
                     <DishCard dish={dish} key={i} col="col-3 col-6-sm" />
-                    <Button
-                    label="Supprimer le plat"
-                    handleClick={() => deleteDish(dish.id)}
-                    href="#"
-                    type="danger"
-                    />
+                    <DeleteDish id={dish.id} onDelete={handleDishUpdate}/>
                 </div>
             ))}
-
         </div>
     );
 };
